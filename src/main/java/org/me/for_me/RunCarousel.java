e9fe236ba00bf71eb4b14cc8d0be403dd88abba2
Alekseii Guskov <ingrzhd@gmail.com>
2023-04-19 16:41:40 +0300

    Project for me and my friends

---
+++ src/main/java/org/me/for_me/RunCarousel.java
@@ -0,0 +1,48 @@
+package org.me.for_me;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+
+public class RunCarousel {
+    static ArrayList<String> allFilms = new ArrayList<>();
+    static String outString;
+    static int nowChoiceNumber;
+    public static void main(String[] args) throws IOException {
+        createFileWithFilmForWatchNow();
+        createFileWithListOfFilmsToWatchNext();
+    }
+    public static String createListOfFileForWatch () throws FileNotFoundException {
+        File filmsListForWatch = new File("Films.txt");
+        Scanner in = new Scanner(filmsListForWatch);
+        while ((in.hasNextLine())) {
+            allFilms.add(in.nextLine());
+        }
+        Random ChoiceNumberOfRandom = new Random();
+        nowChoiceNumber = ChoiceNumberOfRandom.nextInt(allFilms.size());
+        String nowFilmWatch = allFilms.get(nowChoiceNumber);
+        in.close();
+        System.out.println(nowFilmWatch);
+        return nowFilmWatch;
+    }
+    public static void createFileWithFilmForWatchNow () throws IOException {
+        FileWriter newFileWithOneFilmForWatchNow = new FileWriter("NowWatch.txt");
+        newFileWithOneFilmForWatchNow.write(createListOfFileForWatch());
+        newFileWithOneFilmForWatchNow.close();
+    }
+    public static void createFileWithListOfFilmsToWatchNext () throws IOException {
+        FileWriter newFileWithListOfFilmsToWatchNext = new FileWriter("Films.txt");
+        outString = "";
+        for (int i = 0; i < allFilms.size(); i++) {
+            if (i != nowChoiceNumber && i != allFilms.size()-1) {
+                outString += allFilms.get(i) + "\n";
+            }
+            if (i != nowChoiceNumber && i == allFilms.size()-1) {
+                outString += allFilms.get(i);
+            }
+        }
+        newFileWithListOfFilmsToWatchNext.write(outString);
+        newFileWithListOfFilmsToWatchNext.close();
+    }
+}
