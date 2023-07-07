22340eb137d6c194067b82429436552b57bef007
Alekseii Guskov <ingrzhd@gmail.com>
2023-07-07 22:29:28 +0300

    Игра Эндера;

--- src/main/java/org/me/for_me/RunCarousel.java
+++ src/main/java/org/me/for_me/RunCarousel.java
@@ -2,18 +2,20 @@ package org.me.for_me;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class RunCarousel {
     static ArrayList<String> allFilms = new ArrayList<>();
     static String outString;
     static int nowChoiceNumber;
+
     public static void main(String[] args) throws IOException {
         createFileWithFilmForWatchNow();
         createFileWithListOfFilmsToWatchNext();
     }
-    public static String createListOfFileForWatch () throws FileNotFoundException {
+
+    public static String createListOfFileForWatch() throws FileNotFoundException {
         File filmsListForWatch = new File("Films.txt");
         Scanner in = new Scanner(filmsListForWatch);
         while ((in.hasNextLine())) {
@@ -26,19 +28,21 @@ public class RunCarousel {
         System.out.println(nowFilmWatch);
         return nowFilmWatch;
     }
-    public static void createFileWithFilmForWatchNow () throws IOException {
+
+    public static void createFileWithFilmForWatchNow() throws IOException {
         FileWriter newFileWithOneFilmForWatchNow = new FileWriter("NowWatch.txt");
         newFileWithOneFilmForWatchNow.write(createListOfFileForWatch());
         newFileWithOneFilmForWatchNow.close();
     }
-    public static void createFileWithListOfFilmsToWatchNext () throws IOException {
+
+    public static void createFileWithListOfFilmsToWatchNext() throws IOException {
         FileWriter newFileWithListOfFilmsToWatchNext = new FileWriter("Films.txt");
         outString = "";
         for (int i = 0; i < allFilms.size(); i++) {
-            if (i != nowChoiceNumber && i != allFilms.size()-1) {
+            if (i != nowChoiceNumber && i != allFilms.size() - 1) {
                 outString += allFilms.get(i) + "\n";
             }
-            if (i != nowChoiceNumber && i == allFilms.size()-1) {
+            if (i != nowChoiceNumber && i == allFilms.size() - 1) {
                 outString += allFilms.get(i);
             }
         }
