22200bde20b8328a72a9dda2eddc90a90023a9e7
Alekseii Guskov <ingrzhd@gmail.com>
2023-10-05 23:18:58 +0300

    Молодой челловек (2022)

--- src/main/java/org/me/for_me/RunCarousel.java
+++ src/main/java/org/me/for_me/RunCarousel.java
@@ -1,6 +1,9 @@
 package org.me.for_me;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
